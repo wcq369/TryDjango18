a05703a12afd00e36e0d878c81097ccc67e8b81a
CQ <chunqiu.wang@outlook.com>
2016-06-07 20:59:44 -0700

    template added

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "timestamp", "updated"]
+	class Meta:
+		model = SignUp
+
+admin.site.register(SignUp, SignUpAdmin)
